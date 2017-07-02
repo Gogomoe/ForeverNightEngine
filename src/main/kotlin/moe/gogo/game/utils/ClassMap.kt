9accb516a2477440a9198fd18fbb57f416e2f083
Gogo <gogo81745.moe@gmail.com>
2017-07-02 21:26:03 +0800

    ClassMap

---
+++ src/main/kotlin/moe/gogo/game/utils/ClassMap.kt
@@ -0,0 +1,43 @@
+package moe.gogo.game.utils
+
+import kotlin.collections.MutableMap.MutableEntry
+import kotlin.reflect.KClass
+
+
+class ClassMap<T : Any> : MutableMap<KClass<out T>, T> {
+
+    private val map = mutableMapOf<KClass<out T>, T>()
+
+    override val size: Int
+        get() = map.size
+
+    override fun containsKey(key: KClass<out T>) = map.containsKey(key)
+    inline fun <reified E : T> containsKey() = containsKey(E::class)
+
+    override fun containsValue(value: T) = map.containsValue(value)
+
+    override fun get(key: KClass<out T>) = map.get(key)
+    inline fun <reified E : T> get() = get(E::class)
+
+    override fun isEmpty() = map.isEmpty()
+
+    override val entries: MutableSet<MutableEntry<KClass<out T>, T>>
+        get() = map.entries
+
+    override val keys: MutableSet<KClass<out T>>
+        get() = map.keys
+
+    override val values: MutableCollection<T>
+        get() = map.values
+
+    override fun clear() = map.clear()
+
+    override fun put(key: KClass<out T>, value: T): T? = map.put(key, value)
+    inline fun <reified E : T> put(value: E) = put(E::class, value)
+
+    override fun putAll(from: Map<out KClass<out T>, T>) = map.putAll(from)
+
+    override fun remove(key: KClass<out T>): T? = map.remove(key)
+    inline fun <reified E : T> remove() = remove(E::class)
+
+}
---
+++ src/test/kotlin/moe/gogo/game/utils/ClassMapTest.kt
@@ -0,0 +1,80 @@
+package moe.gogo.game.utils
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.properties.Gen
+import io.kotlintest.specs.StringSpec
+import moe.gogo.test.`in`
+
+class ClassMapTest : StringSpec() {
+
+    init {
+        "put"{
+            val map = createNumberClassMap()
+            val numbers: Set<Number> = generateAllTypeNumbers()
+            numbers.forEach { map.put(it::class, it) }
+
+            map.size shouldBe numbers.size
+            numbers.forEach {
+                map[it::class] shouldBe it
+            }
+        }
+
+        "put and cover value"{
+            val map = createNumberClassMap()
+            val i: Int = Gen.int().generate()
+            (1..10).forEach { map.put(Gen.int().generate()) }
+            map.put(i)
+
+            map.size shouldBe 1
+            map.get<Int>() shouldBe i
+        }
+
+        "remove and contains key"{
+            val map = createNumberClassMap()
+            val numbers: Set<Number> = generateAllTypeNumbers()
+            numbers.forEach { map.put(it::class, it) }
+
+            map.remove<Double>()
+            map.size shouldBe 3
+            map.containsKey<Double>() shouldBe false
+
+            map.clear()
+            map.isEmpty() shouldBe true
+        }
+        "keys, values and entries"{
+            val map = createNumberClassMap()
+            val numbers: Set<Number> = generateAllTypeNumbers()
+            numbers.forEach { map.put(it::class, it) }
+
+            numbers.forEach {
+                it::class shouldBe `in`(map.keys)
+                it shouldBe `in`(map.values)
+            }
+
+            map.entries.forEach { (_, v) ->
+                v shouldBe `in`(numbers)
+            }
+        }
+        "put all values"{
+            val map = createNumberClassMap()
+            val empty = createNumberClassMap()
+            val numbers: Set<Number> = generateAllTypeNumbers()
+            numbers.forEach { map.put(it::class, it) }
+
+            empty.putAll(map)
+
+            numbers.forEach {
+                empty.containsValue(it)
+            }
+        }
+    }
+
+    fun createNumberClassMap() = ClassMap<Number>()
+    fun generateAllTypeNumbers(): Set<Number> {
+        val i: Int = Gen.int().generate()
+        val f: Float = Gen.float().generate()
+        val d: Double = Gen.double().generate()
+        val l: Long = Gen.long().generate()
+        return setOf(i, f, d, l)
+    }
+}
